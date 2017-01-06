c40862c732648c8b35f221890f71cd1b581c1d81
Shireef Khatab <sherif_19@hotmail.co.uk>
2017-01-06 14:07:59 +0000

    add bootstrap, font-awesome, edit some class names

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 		var counter = 0;
 		var score = 0;
 		var answer = "";
-		var check = "";
+		var check_anwser = "";
 		var final_score = 0;
 		var finalPage = 11;
 		var audio1 = new Audio('audio/correct.mp3');
@@ -12,40 +12,45 @@
 
 /***************************************** when next btn is clicked *****************************/
 		$('#next').click(function(){
-			if(counter <1){$('.start').hide();}
+			if(counter < 1){
+				$('.start').hide();
+			}
 			$('.form').hide();
-			counter++;	console.log(counter);
-			$('#form'+counter+', #back').slideDown(1500);
+			
+			counter++;
+			$('#form' + counter + ', #back').slideDown(1000);
+			
 			// last page/
 			if(counter >= finalPage){
 				$('#next').hide();
 				$('.your-result').css('color','lightgreen').slideDown('slow');
 				counter = finalPage;
+			
 			// result/
 				if(final_score < 100){
 					$('.mess').css('color','red').slideDown(1500);
-				}else{
+				} else {
 					$('.mess').hide();
 					if (score < 50){
 						$('.fail').slideDown(1500);
 						$('.total').css('color','red').text(' '+score);
-						$('.final-score').css('color','pink').text(' '+final_score);
-					}else{
+						$('.final-score').css('color','lightblue').text(' '+final_score);
+					} else {
 						$('.congrats').slideDown(1500);
 						$('.review').css('color','yellow').slideDown(1500);
-						$('.or').css('color','white').slideDown(1500);
+						$('.restart').css('color','white').slideDown(1500);
 						$('.total').css('color','lightblue').text(' '+score);
-						$('.final-score').css('color','pink').text(' '+final_score);
-					} // end of last else/		
-				} // end of first else/
-			} // end of first if/
-		}); // end of next btn click/
+						$('.final-score').css('color','lightblue').text(' '+final_score);
+					} // end if/		
+				} // end if/
+			} // end if/
+		});
 /*********************************************** when back btn is clicked ***************************/
 		$('#back').click(function(){
 			$('.form').hide();
-			counter--;	console.log(counter);
+			counter--;
 			$('#form'+counter+',#next ').slideDown(1500);
-			$('.final-title, .col-md-12').children().hide();
+			$('.final-title, .final-info').children().hide();
 			if(counter <= 1){
 				$('#back').hide();
 				$('.start').hide();
@@ -56,19 +61,19 @@
 		$('.form').on('click','button',function(event){
 			event.preventDefault();
 			event.stopPropagation();
-			var clicked=$(this).attr('id');
-			var answer= $(this).closest('.form').find('.correct').text();
+			var clicked = $(this).attr('id');
+			var answer = $(this).closest('.form').find('.correct').text();
 			// when answer is correct/
 			if(answer === clicked){
 				score += 10;
-				check_counter = 'Correct';
-				$('.result-'+counter).css('color','lightblue').text(check_counter);
+				check_anwser = 'Correct';
+				$('.result-'+counter).css('color','lightblue').text(check_anwser);
 				$('#your-answer-'+counter).css('color','lightblue').html($(this).text());
 				audio1.play();
 			}else{
 			// when answer is incorrect/
-				check_counter = 'Incorrect!'
-				$('.result-'+counter).css('color','red').text(check_counter);
+				check_anwser = 'Incorrect!'
+				$('.result-'+counter).css('color','red').text(check_anwser);
 				$('#your-answer-'+counter).css('color','red').html($(this).text());
 				audio2.play();
 			}
